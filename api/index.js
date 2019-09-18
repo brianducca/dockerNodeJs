7688bfcd6e8b751cdfe306aae5b0b342a17e8154
Brian Ducca <bducca@agn.gov.ar>
2019-09-18 16:15:53 -0300

    Modifico mensaje

--- api/index.js
+++ api/index.js
@@ -4,9 +4,9 @@ var app = express();
 app.use(express.json());
 
 app.get('/', function(req, res) {
-    res.send("Docker asasdasdsddasd");
+    res.send("Docker con livereload");
 });
 
 app.listen(3000, function(req, res) {
-    console.log("API funcionando en el puerto 3000");
+    console.log("API funcionando en el puerto 4005 externo y 3000 para docker");
 });
